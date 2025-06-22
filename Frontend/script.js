a710ce17b1ead3cb02949215760a0ef3b30cc55e
Sino57 <sinarfayyad5@gmail.com>
2025-06-22 20:51:40 +0300

    draft 28
    organizing the hidden sections

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,18 +1,38 @@
-function openSection(sectionId) {
-const allSections = document.querySelectorAll("section");
-allSections.forEach(section => {
-      section.style.display = "none";
-});
-
-let section = document.getElementById(sectionId);
-section.style.display = "block"; 
+function toggleSettings() {
+    const settings = document.getElementById("settings_page");
+    if (settings.style.display == "block") {
+        settings.style.display = "none";
+    }else{
+        settings.style.display = "block";
+    }
 }
 
-function toggleSettings() {
-    const settingsSection = document.getElementById('settings_page');
-    if (settingsSection.classList.contains('show')) {
-        settingsSection.classList.remove('show');
-    } else {
-        settingsSection.classList.add('show');
+function toggleUpdateProfile(){
+    const settings = document.getElementById("settings_page");
+    if (settings.style.display == "block") {
+        settings.style.display = "none";
+    }
+
+
+    const updateProfile = document.getElementById("update_profile");
+    if (updateProfile.style.display == "block") {
+        updateProfile.style.display = "none";
+    }else{
+        updateProfile.style.display = "block";
+    }
+}
+
+function toggleLogout() {
+
+    const settings = document.getElementById("settings_page");
+    if (settings.style.display == "block") {
+        settings.style.display = "none";
+    }
+
+    const logout = document.getElementById("logout_page");
+    if (logout.style.display == "block") {
+        logout.style.display = "none";
+    }else{
+        logout.style.display = "block";
     }
 }
