1c18c11b8cfacff09f1530649921cb2308bc32d2
Sino57 <sinarfayyad5@gmail.com>
2025-06-22 10:52:35 +0300

    draft 18
    changing the way ( setting_page) shows

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,9 +1,20 @@
 function openSection(sectionId) {
 const allSections = document.querySelectorAll("section");
 allSections.forEach(section => {
-    section.style.display = "none"; 
+    if (section.id!== "settings_page") {
+      section.style.display = "none";
+}
 });
 
 let section = document.getElementById(sectionId);
 section.style.display = "block"; 
+}
+
+function toggleSettings() {
+    const settingsSection = document.getElementById('settings_page');
+    if (settingsSection.classList.contains('show')) {
+        settingsSection.classList.remove('show');
+    } else {
+        settingsSection.classList.add('show');
+    }
 }
